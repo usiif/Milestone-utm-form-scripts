357bdff3588f52fcaf2701a0433953f982aafb6c
Youssef Ahmed <136795880+usiif@users.noreply.github.com>
2025-06-19 14:17:23 +0300

    Create Coverstion Scripts.js

---
+++ Coverstion Scripts.js
@@ -0,0 +1,55 @@
+<!-- Global Site Tag (gtag.js) - Google Ads GOES AT THE TOP -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-841566858"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag() { dataLayer.push(arguments); }
+  gtag('js', new Date());
+  gtag('config', 'AW-841566858');
+</script>
+
+<script>
+  document.addEventListener("DOMContentLoaded", function () {
+    // WhatsApp Click
+    const whatsapp = document.getElementById("whatsappBtn"); //PLEASE UPDATE THE ID TO MATCH 
+    if (whatsapp) {
+      whatsapp.addEventListener("click", function (e) {
+        e.preventDefault();
+        const url = whatsapp.href;
+        gtag('event', 'conversion', {
+          'send_to': 'AW-841566858/8-YYCKji7_gYEIqVpZED',
+          'event_callback': function () {
+            window.location = url;
+          }
+        });
+      });
+    }
+
+    // Phone Click
+    const phone = document.getElementById("callBtn"); // PLEASE UPDATE THE ID TO MATCH
+    if (phone) {
+      phone.addEventListener("click", function (e) {
+        e.preventDefault();
+        const url = phone.href;
+        gtag('event', 'conversion', {
+          'send_to': 'AW-841566858/ZHR5CMWZvfIYEIqVpZED',
+          'event_callback': function () {
+            window.location = url;
+          }
+        });
+      });
+    }
+
+    // Zoho Form Submission
+    window.addEventListener('message', function (event) {
+      const evntData = event.data;
+      if (typeof evntData === 'string') {
+        const zf_data = evntData.split("|");
+        if (zf_data.length === 3 && zf_data[2] === 'sent') {
+          gtag('event', 'conversion', {
+            'send_to': 'AW-841566858/6QcwCMKZvfIYEIqVpZED'
+          });
+        }
+      }
+    }, false);
+  });
+</script>
